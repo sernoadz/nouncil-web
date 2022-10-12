41f11cde29e6d0ebea2342eb928a0a7cb41ec071
SerNoadz <thenoadz@gmail.com>
2022-10-12 13:25:07 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
-  async redirects() {
-    return [
-      {
-        source: '/gov',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
